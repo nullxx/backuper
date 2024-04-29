dbe74155f1f574488cc36b4432716b7b7cd660e6
Jon Lara <jon.laratrigo@gmail.com>
2024-04-29 11:23:09 +0200

    fix: once database initialized and test ok, don't let edit db config without authorization

--- src/api/middlewares/auth.ts
+++ src/api/middlewares/auth.ts
@@ -1,4 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
+import { ConfigType, readConfig } from '../../lib/fsconfig';
+import { APIError } from '../error/APIError';
+import Logger from '../../lib/logger';
+
+const logger = Logger();
 
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
     if (req.session.user) {
@@ -10,10 +15,13 @@ export function requireAuth(req: Request, res: Response, next: NextFunction) {
     }
 }
 
-export function requireAuthSoft(req: Request, res: Response, next: NextFunction) {
+export async function requireAuthSoft(req: Request, res: Response, next: NextFunction) {
     // only require auth if database is set
     if (req.app.locals.dbExists && req.app.locals.testDbOk) {
         requireAuth(req, res, next);
+    } else if ((await readConfig(ConfigType.DB_INITIALIZED))?.initialized){
+        next(new APIError(`Database is configured but connection test failed. Please check the logs`, true));
+        logger.error(`Database is configured but connection test failed: please check database is up and running, or reconfigure the database connection at ${ConfigType.DB}`);
     } else {
         next();
     }
